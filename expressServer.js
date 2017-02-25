6d6e9b9cf23f75e6de3c971538591f7753cc035c
Ivonne Terrero <iqcodes21@gmail.com>
2017-02-25 11:24:11 -0800

    Send individual pet record from database

--- expressServer.js
+++ expressServer.js
@@ -11,27 +11,51 @@ let port = process.env.PORT || 8000;
 
 //route to pets
 // let pets = '/pets.json'
+app.disable('x-powered-by');
 
 app.get('/pets', function(req, res){
   fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
     // console.log(typeof petsJSON);
-    let petIndex = req.params.id;
+    // let petIndex = req.params.id;
     // let numPets = pets.length;
     if(readErr) {
-      throw readErr;
+      // throw readErr;
+       console.error(readErr.stack);
       return res.sendStatus(500);
     }
-    let responsePet = JSON.parse(petsJSON); //petsJSON was a string format.
+    let responsePets = JSON.parse(petsJSON); //petsJSON was a string format.
     // console.log(typeof responsePet);
-    res.send(responsePet);
+    res.send(responsePets);
   // console.log(curPets);
   });
 });
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
+    if (readErr) {
+      console.error(readErr.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var responsePets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= responsePets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(responsePets[id]);
+  });
+});
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
 app.listen(port, function() {
-  console.log('we are succesfully running ! yahhh!');
+  console.log('we are succesfully running ! yahhh!', port);
 });
 
-//
+
+//another way to writting the second part of the request.
 // app.get('/pets/:id', function(req, res){
 //   var petIndex = req.params.id;
 //   var numPets = pets.length;
