3fe85fe2354cb0a3b3dee2736d853ac469b69769
Ivonne Terrero <iqcodes21@gmail.com>
2017-02-26 17:32:56 -0800

    refactoring my code

--- expressServer.js
+++ expressServer.js
@@ -3,51 +3,54 @@
 let fs = require('fs');
 let path = require('path');
 let petsPath = './pets.json';
-//could have also written it as follo:
+//could have also written it as follow:
  // let petsPath = path.join(__dirname, 'pets.json');
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 8000;
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//route to pets
-// let pets = '/pets.json'
 app.disable('x-powered-by');
 
-app.get('/pets', function(req, res){
+app.get('/pets', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
     // console.log(typeof petsJSON);
     // let petIndex = req.params.id;
     // let numPets = pets.length;
     if(readErr) {
-      // throw readErr;
-       console.error(readErr.stack);
+      console.error(readErr.stack);
       return res.sendStatus(500);
     }
-    let responsePets = JSON.parse(petsJSON); //petsJSON was a string format.
-    // console.log(typeof responsePet);
+    let responsePets = JSON.parse(petsJSON);
+    res.set('Content-Type', 'application/json');
     res.send(responsePets);
-  // console.log(curPets);
   });
 });
 // allowing server to send individual records based on the path and the item located
 //at the id. index of the array.
 //NOTE: the /:id (route parameter) is adding a dynamic route to the application. NOTE: id is a variable
 //name this can be anything.
+
 app.get('/pets/:id', function(req, res) {
-  fs.readFile(petsPath, function(readErr, petsJSON) {
+  fs.readFile(petsPath, 'utf8', function(readErr, petsJSON) {
     if (readErr) {
       console.error(readErr.stack);
       return res.sendStatus(500);
     }
 
     let id = parseInt(req.params.id);
-    //giving a variable name to the route of req.params.id
     let responsePets = JSON.parse(petsJSON);
 
-    if (id < 0 || id >= responsePets.length || isNaN(id)) {
-      return res.sendStatus(404);
+    if (!(responsePets[id])){
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(404);
     }
-
+    if (id < 0 || id >= responsePets.length || isNaN(id)) {
+      res.set('Content-Type', 'text/plain');
+      res.sendStatus(404);
+    } else {
     res.set('Content-Type', 'application/json');
     res.send(responsePets[id]);
   });
@@ -56,27 +59,25 @@ app.get('/pets/:id', function(req, res) {
 // note might want to use bodyParser() and milter. //These are middleware.
   app.post('/pets', function(req, res){
     fs.readFile(petsPath, function(readErr, petsJSON) {
-      if(readErr) {
+      let curPetsArr = JSON.parse(petsJSON);
+      let givenPet = {'age': age, 'kind':kind, 'name': name};
+      let age = parseInt(req.body.age);
+      let kind = req.body.kind;
+      let name = req.body.name;
+
+      if (readErr) {
         console.error(readErr.stack);
         return res.sendStatus(400);
       }
-      // creating the class of the req that must be passed.
-          let curPets = JSON.parse(petsJSON);
-          let givenPet = req.body;
-          //  console.log(givenPet);
-          if(!givenPet){
-            return res.sendStatus(400);
-          }
-          //simplifying the given body values;
-          let age = parseInt(req.body.age);
-          let kind = req.body.kind;
-          let name = req.body.name;
-          // req.body.pro appeds
-          //creatring the object.
-      if ( age && kind && name ) {
-        curPets.push(givenPet);
+      if(!givenPet) {
+        console.error("Needs more information")
+        return res.sendStatus(400);
       }
-        let newPetsJSON = JSON.stringify(curPets);
+
+      if ( age && kind && name ) {
+        curPetsArr.push(givenPet);
+      }
+      let newPetsJSON = JSON.stringify(curPetsArr);
 
         fs.writeFile(petsPath, newPetsJSON, function(writeErr) {
           if(writeErr) {
@@ -84,23 +85,15 @@ app.get('/pets/:id', function(req, res) {
             return res.sendStatus(500);
           }
           res.set('Content-Type', 'application/json');
-          res.send(responsePets[id]);
-          // console.log(givenPet);
+          res.send(responsePets);
         });
-      // }
     });
-  })
-app.use(function(req, res) {
+  });
+app.use(function(req, res, next) {
   res.sendStatus(404);
 });
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
-
-
-
-// exporting the server.
-
-//attempting the bonus.
 
 module.exports = app;
